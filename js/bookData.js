b8344ec9c04eb10d527d9acb91a4c8a98bba54c8
TykeaLy <tykeaboyloy@gmail.com>
2021-10-02 15:14:32 +0700

    sdfsf

--- js/bookData.js
+++ js/bookData.js
@@ -93,3 +93,18 @@ bookInfor.map((item, index) => {
 function filter(x) {
   document.getElementsByClassName("textArrival")[0].innerHTML = x;
 }
+bookInfor = JSON.parse(localStorage.getItem("books"));
+console.log(bookInfor);
+var getElement = document.getElementsByClassName("pic")[0];
+bookInfor.map((item) => {
+  getElement.innerHTML =
+    getElement.innerHTML +
+    `
+          <div class="hi">
+              <a href="/html/bookDetail.html" target="_blank"> <img src="${item.bookLink}" alt="book"></a>
+              <h4>Title: ${item.bookTitle}</h4>
+              <span>Author: ${item.bookAuthor}</span><br>
+              <span><b style="color:red;">Price from : ${item.bookPrice}</b></span>
+          </div>
+    `;
+});
